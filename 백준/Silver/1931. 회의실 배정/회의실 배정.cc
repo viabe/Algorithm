f7cd6f5858db6d1af63922283b429df148c57183
viabe <98133327+viabe@users.noreply.github.com>
2025-03-21 15:05:09 +0900

    [Silver I] Title: 회의실 배정, Time: 24 ms, Memory: 2808 KB -BaekjoonHub

--- 백준/Silver/1931. 회의실 배정/회의실 배정.cc
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.cc
@@ -1,54 +1,51 @@
-#include<iostream>
-#include <algorithm>
+#include <iostream>
 #include <vector>
+#include <algorithm>
 using namespace std;
 
-struct Meeting
-{
-	int firstTime; //시작시간
-	int lastTime; //종료시간
+	//문제 풀이 : 한개의 회의실 이를 사용하고자 하는 N개의 회의에 대하여 회의실 사용표를 만들어야함
+	//다만 각 회의는 시작 시간과 끝나는 시간이 주어져 있고 각 회의가 겹치지 않으면서도
+	//회의를 최대한 많이 할 수 있는 개수를 세어야한다.
+	//일단 시작시간과 마지막 시간이 별로 차이나지 않는 회의를 골라야한다.
+	//즉, 마지막 시간이 가장 작은 회의를 골라야한다는 뜻이다.
+	//우선 내부의 배열을 정렬하고, 시작 시간 순이지만 마지막 시간도 적게 정렬해야한다.
+
+struct Meeting{
+	int StartTime;
+	int EndTime;
 };
 
-//오름차순으로 종료시간을 기준으로 해야함
-bool compare(Meeting a, Meeting b) {
-	if (a.lastTime == b.lastTime) {
-		return a.firstTime < b.firstTime;
+bool compare(Meeting a, Meeting b){
+	if(a.EndTime == b.EndTime){
+		return a.StartTime < b.StartTime;
 	}
-	return a.lastTime < b.lastTime;
+	return a.EndTime < b.EndTime;
 }
 
 int main() {
 	ios_base::sync_with_stdio(0);
-	cin.tie(0);
 	cout.tie(0);
-
-	//회의의 개수
-	int n;
+	cin.tie(0);
+	
+	int n; //회의의 총 개수
 	cin >> n;
 
-	//회의 시간 넣을 배열
-	vector <Meeting> t(n);
-
-	for (int i = 0; i < n; i++) {
-		cin >> t[i].firstTime >> t[i].lastTime;
+	vector<Meeting> m(n);
+	for(int i =0 ; i<n; i++){
+		cin >> m[i].StartTime >> m[i].EndTime;	
 	}
 
-	//정렬쓰기 첫번째수를 기준으로 
-	sort(t.begin(), t.end(), compare);
+	sort(m.begin(), m.end(), compare); //정렬
 
-	//최대 사용할 수 있는 회의의 개수 일단은 처음꺼는 있기 때문에 1을 기본으로
-	int result = 1;
+	int result = 1; //회의의 최대 개수, 하나는 무조건 있기 때문에
+	int endTime = m[0].EndTime; //끝나는 시간을 맞춰서 넣기
 
-	//마지막 시간은 계속해서 변경되어야하기 때문에 변수 필요
-	int endTime = t[0].lastTime;
-
-	//이제 lasttime이 작은 순번을 골라서 카운팅
-	//근데 1부터 시작해야함 이미 result를 1했으니까
-	for (int i = 1; i < n; i++) {
-		//가장 처음 배열 즉 t[0]을 시작으로 t[i].fistTime 의 수를 찾아야함. 가장 작은 걸 빨리 찾아야함
-		if(t[i].firstTime >= endTime) {
+	//끝나는 시간을 기점으로 골라야함
+	for(int i = 1; i<n; i++){
+		//첫번째 시간이 endTime에 가장 근접한 수를 찾아야함 이미 정렬을 했기 때문에 수만 찾으면 끝
+		if(m[i].StartTime >= endTime){
+			endTime = m[i].EndTime;
 			result++;
-			endTime = t[i].lastTime;
 		}
 	}
 
